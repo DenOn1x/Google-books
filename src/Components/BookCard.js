1740d876e4efcac8ed89b24fb496c8a709a4837e
Denis Onix <den.onix2002@gmail.com>
2022-08-04 01:11:15 +0300

    [ADD] volume : properties of the same type are moved to constants

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -4,16 +4,17 @@ const BookCard = ({book}) => {
     const openCard = (e) => {
         window.open(e.volumeInfo.infoLink, '_blank');
     }
+    const volume = book.volumeInfo;
     return (
         <div className="book-card" onClick={() => openCard(book)}>
-            {book.volumeInfo.imageLinks &&
-                <img className="book-image" src={book.volumeInfo.imageLinks.thumbnail} alt="Picture didn't load"/>}
-                <p className="book-category">{(book.volumeInfo?.categories || 'Not category')}</p>
-                <p className="book-title">{book.volumeInfo.title}</p>
-                <p className="book-author"><b>Author:</b> {book.volumeInfo.authors}</p>
+            {volume.imageLinks &&
+                <img className="book-image" src={volume.imageLinks.thumbnail} alt="Picture didn't load"/>}
+                <p className="book-category">{(volume?.categories || 'Not category')}</p>
+                <p className="book-title">{volume.title}</p>
+                <p className="book-author"><b>Author:</b> {volume.authors}</p>
                 <p className="book-date-published">
-                    <b>Published Date:</b> {book.volumeInfo.publishedDate === '0000' ? 'Not available' :
-                    book.volumeInfo.publishedDate.substring(0, 4)}
+                    <b>Published Date:</b> {volume.publishedDate === '0000' ? 'Not available' :
+                    volume.publishedDate.substring(0, 4)}
                 </p>
         </div>
     );
